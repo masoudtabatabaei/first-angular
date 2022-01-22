6e7623805f73a9f104a4505abb0cfa71b0e208d3
Masoud Tabatabaei <mas.tabatabaei@ad.mydigipay.com>
2022-01-22 14:18:05 +0330

    Learning: Create component with CLI and test different states for Selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent ,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
