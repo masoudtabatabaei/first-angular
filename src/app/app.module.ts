5387b8e62b365617fd1539906d6f5b4854709e8d
Masoud Tabatabaei <mas.tabatabaei@ad.mydigipay.com>
2022-01-22 11:11:37 +0330

    Learning:Create and use ServerComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent ,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-server' ,
+  templateUrl : './server.component.html' ,
+  styleUrls : []
+})
+export class ServerComponent {
+  title = 'serverComponentTitle';
+}
