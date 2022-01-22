b193345be502825174cbae69cb6c88d07555813c
Masoud Tabatabaei <mas.tabatabaei@ad.mydigipay.com>
2022-01-22 15:28:33 +0330

    Learning: Create WarningAlertComponent and SuccessAlertComponent and use them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent ,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-warning-alert' ,
+  templateUrl : './warning-alert.component.html',
+  styles : [`
+    div {
+      padding: 20px;
+      background-color: rosybrown;
+      border: 1px solid red;
+    }
+  `]
+})
+export class WarningAlertComponent {}
