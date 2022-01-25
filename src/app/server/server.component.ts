56a65bdaa101d97978f21d63c71aefec8cb47d35
Masoud Tabatabaei <masoudtabatabaei20@gmail.com>
2022-01-25 10:17:09 +0330

    Learning: work with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,9 +8,17 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverId: number = Math.floor(Math.random() * 8);
   serverIp: string = "192.168.1.1";
-  serverStatus: string = "Offline";
+  serverStatus: string = "";
+
+  constructor() {
+    this.serverStatus = Math.random() < 0.5 ? 'Offline' : 'Online';
+  }
 
   getServerInfo() {
     return `Server info => id:${this.serverId} and ip: ${this.serverIp} and status: ${this.serverStatus}`;
+  }
+
+  getServerColor() {
+    return this.serverStatus === 'Offline' ? "red" : "green";
   }
 }
