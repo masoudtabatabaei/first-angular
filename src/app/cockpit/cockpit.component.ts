e57e4406950f24b61aa427b14e60fe74ae63bed9
Masoud <masoudtabatabaei20@gmail.com>
2022-03-08 22:24:25 +0330

    generate cockpit component

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { ServerElementModel } from '../_models/serverElement.model';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent implements OnInit {
+  serverName = '';
+  serverContent = '';
+
+  @Output()
+  addedItemEvent:EventEmitter<ServerElementModel> = new EventEmitter<ServerElementModel>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddBlueprint() {
+    this.addedItemEvent.emit({
+      type: "blueprint" ,
+      name: this.serverName ,
+      content: this.serverContent
+    });
+  }
+
+  onAddServer() {
+    this.addedItemEvent.emit({
+      type: "server" ,
+      name: this.serverName ,
+      content: this.serverContent
+    });
+  }
+}
